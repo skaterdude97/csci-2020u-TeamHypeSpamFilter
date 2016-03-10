b56c0aa419a2bce706ba95bfe755c56a132ab56d
Andrei Stoica <andrei.stoica@uoit.net>
2016-03-10 16:41:30 -0500

    completed the UI code and fixed testing class

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,36 +1,93 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
-
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 public class Main extends Application {
+    private BorderPane layout;
+    private TableView<TestFile> table;
+    private TextField accuracy, precision;
+    private GridPane summary;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Spam Defender >9000");
         DirectoryChooser directoryChooser = new DirectoryChooser();
         directoryChooser.setInitialDirectory(new File("."));
         File mainDirectory = directoryChooser.showDialog(primaryStage);
+
+        Training trainer = new Training();
+        trainer.train(new File (mainDirectory + "/train"));
+
+        Testing tester = new Testing(new File(mainDirectory + "/test"));
+        tester.test(trainer.getProbOfWord());
+
+        TableColumn<TestFile, String> fileNameColumn = null;
+        fileNameColumn = new TableColumn<>("File");
+        fileNameColumn.setMinWidth(200);
+        fileNameColumn.setCellValueFactory(new PropertyValueFactory<>("filename"));
+        fileNameColumn.setCellFactory(TextFieldTableCell.<TestFile>forTableColumn());
+
+        TableColumn<TestFile, String> actualClassColumn = null;
+        actualClassColumn = new TableColumn<>("Actual Class");
+        actualClassColumn.setMinWidth(80);
+        actualClassColumn.setCellValueFactory(new PropertyValueFactory<>("actualClass"));
+        actualClassColumn.setCellFactory(TextFieldTableCell.<TestFile>forTableColumn());
+
+        TableColumn<TestFile, Double> probabilityColumn = null;
+        probabilityColumn = new TableColumn<>("Spam Probability");
+        probabilityColumn.setMinWidth(200);
+        probabilityColumn.setCellValueFactory(new PropertyValueFactory<>("spamProbability"));
+
+        table = new TableView<>();
+        table.setItems(tester.getFiles());
+
+        table.getColumns().add(fileNameColumn);
+        table.getColumns().add(actualClassColumn);
+        table.getColumns().add(probabilityColumn);
+
+        summary = new GridPane();
+        summary.setPadding(new Insets(10,10,10,10));
+        summary.setVgap(10);
+        summary.setHgap(10);
+
+        Label accuracyLabel = new Label("Accuracy");
+        summary.add(accuracyLabel,0,0);
+        accuracy = new TextField();
+        accuracy.setEditable(false);
+        accuracy.setText(""+ tester.getAccuracy());
+        summary.add(accuracy,1,0);
+
+        Label precisionLabel = new Label("Precision");
+        summary.add(precisionLabel,0,1);
+        precision = new TextField();
+        precision.setEditable(false);
+        precision.setText(""+ tester.getPrecision());
+        summary.add(precision,1,1);
+
+        layout = new BorderPane();
+        layout.setCenter(table);
+        layout.setBottom(summary);
+
+        Scene scene = new Scene(layout, 800, 600);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
-        Training t = new Training();
-        File dir = new File("/home/brad/Documents/csci-2020u-TeamHypeSpamFilter/src/sample/data/train");
-
-        t.train(dir);
-
         launch(args);
     }
 }
--- src/sample/Testing.java
+++ src/sample/Testing.java
@@ -3,22 +3,30 @@ package sample;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Map;
+import java.util.Observable;
 import java.util.Scanner;
+import javafx.collections.*;
 
 
 /**
  * Created by andrei on 10/03/16.
  */
 public class Testing {
-    private TestFile[] files;
+    private ObservableList<TestFile> files = FXCollections.observableArrayList();
     private File spam, ham;
+    private int correctSpam=0, spamCount=0;
+    private double accuracy, precision;
 
     public Testing(File dir) {
         spam = new File(dir, "/spam");
         ham = new File(dir, "/ham");
-        int numFiles = spam.listFiles().length + ham.listFiles().length;
-        files = new TestFile[numFiles];
     }
+
+    public double getAccuracy () {return accuracy;}
+
+    public double getPrecision () {return precision;}
+
+
 
     private boolean isWord(String str){
         String pattern = "^[a-zA-Z]*$";
@@ -30,35 +38,65 @@ public class Testing {
 
     public void test (Map<String, Double> probOfWord) {
         try {
-            double sp = 0;
+
             for (int i = 0; i<spam.listFiles().length; i++) {
                 Scanner testInput = new Scanner(spam.listFiles()[i]);
+                double sp;
+                double n = 0;
                 while (testInput.hasNext()) {
                     String word = testInput.next();
                     if (isWord(word)) {
                         if (probOfWord.containsKey(word)) {
-                            sp += Math.log(1-probOfWord.get(word))- Math.log(probOfWord.get(word));
+                            if (probOfWord.get(word) == 1) {
+                                n += (Math.log(1 - 0.99999999999) - Math.log(0.9999999999));
+                            } else if (probOfWord.get(word)!= 0) {
+                                n += (Math.log(1 - probOfWord.get(word)) - Math.log(probOfWord.get(word)));
+                            } else {
+                                n+= (Math.log(1 -0.00000000001) - Math.log(0.00000000001));
+                            }
                         }
                     }
                 }
-                sp = 1/(1+Math.pow(Math.E,sp));
+                sp = 1/(1+Math.pow(Math.E,n));
+
+                if (sp>0.5) {correctSpam++;spamCount++;}
 
 
-                files[i] = new TestFile(spam.listFiles()[i].getName(), sp, "spam" );
+                files.add(new TestFile(spam.listFiles()[i].getName(),sp,"Spam"));
             }
             for (int i = 0; i<ham.listFiles().length; i++) {
+                Scanner testInput = new Scanner(ham.listFiles()[i]);
+                double sp;
+                double n = 0;
+                while (testInput.hasNext()) {
+                    String word = testInput.next();
+                    if (isWord(word)) {
+                        if (probOfWord.containsKey(word)) {
+                            if (probOfWord.get(word) == 1) {
+                                n += (Math.log(1 - 0.99999999999) - Math.log(0.9999999999));
+                            } else if (probOfWord.get(word)!= 0) {
+                                n += (Math.log(1 - probOfWord.get(word)) - Math.log(probOfWord.get(word)));
+                            } else {
+                                n += (Math.log(1 - 0.00000000001) - Math.log(0.00000000001));
+                            }
+                        }
+                    }
+                }
+                sp = 1/(1+Math.pow(Math.E,n));
 
+                if (sp>0.5) {spamCount++;}
 
-
-                files[i] = new TestFile(ham.listFiles()[i].getName(), sp, "ham" );
+                files.add(new TestFile(ham.listFiles()[i].getName(), sp,"ham"));
             }
+
+            accuracy = 1-((spamCount-correctSpam)/(double)files.size());
+            precision = correctSpam/(double)spamCount;
         } catch (FileNotFoundException e) {
             System.out.println("File not found.");
         }
-
     }
 
-    public TestFile[] getFiles () {
+    public ObservableList<TestFile> getFiles () {
         return files;
     }
 }
