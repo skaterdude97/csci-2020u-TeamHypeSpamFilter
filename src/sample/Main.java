0a6a91590a7942d1b191a58951881317830ad86c
Brad Gardiner <bradgardiner21@gmail.com>
2016-03-09 21:08:50 -0500

    Updating testing to add File I/O

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,7 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -13,11 +18,29 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setInitialDirectory(new File("."));
+        File mainDirectory = directoryChooser.showDialog(primaryStage);
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+        Training t = new Training();
+        File dataDir = new File("/home/brad/Documents/csci-2020u-TeamHypeSpamFilter/src/sample/data/train");
+        System.out.println("File: " + dataDir);
+
+        try{
+            t.processFile(dataDir);
+            t.printWordCounts(2, new File("wordcount.txt"));
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
         launch(args);
     }
 }
---
+++ src/sample/Training.java
@@ -0,0 +1,86 @@
+package sample;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+/**
+ * Created by brad on 08/03/16.
+ */
+public class Training {
+    private Map<String, Integer> wordCounts;
+
+    public Training() {
+        wordCounts = new TreeMap<>();
+    }
+
+    public void processFile(File file) throws IOException {
+        if (file.isDirectory()) {
+            // process all of the files recursively
+            File[] filesInDir = file.listFiles();
+            for (int i = 0; i < filesInDir.length; i++) {
+                processFile(filesInDir[i]);
+            }
+        } else if (file.exists()) {
+            // load all of the data, and process it into words
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext()) {
+                String word = scanner.next();
+                if (isWord(word)) {
+                    countWord(word);
+                }
+            }
+        }
+    }
+
+    private void countWord(String word) {
+        if (wordCounts.containsKey(word)) {
+            int oldCount = wordCounts.get(word);
+            wordCounts.put(word, oldCount + 1);
+        } else {
+            wordCounts.put(word, 1);
+        }
+    }
+
+    private boolean isWord(String str){
+        String pattern = "^[a-zA-Z]*$";
+        if (str.matches(pattern)){
+            return true;
+        }
+        return false;
+    }
+
+    public void printWordCounts(int minCount, File outputFile) throws FileNotFoundException {
+        System.out.println("Saving word counts to " + outputFile.getAbsolutePath());
+        if (!outputFile.exists() || outputFile.canWrite()) {
+            PrintWriter fout = new PrintWriter(outputFile);
+
+            Set<String> keys = wordCounts.keySet();
+            Iterator<String> keyIterator = keys.iterator();
+
+            while(keyIterator.hasNext()) {
+                String key = keyIterator.next();
+                int count = wordCounts.get(key);
+
+                if (count >= minCount) {
+                    
+                    fout.println(key + ": " + count);
+                }
+            }
+            fout.close();
+        } else {
+            System.err.println("Cannot write to output file");
+        }
+    }
+
+
+
+
+    public void train(){
+
+    }
+}
