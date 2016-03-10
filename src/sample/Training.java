8dc09ebe8604459aa70ec8ed2939ebf6a6b6dd35
Brad Gardiner <bradgardiner21@gmail.com>
2016-03-10 09:25:37 -0500

    Pushing training continued

--- src/sample/Training.java
+++ src/sample/Training.java
@@ -14,14 +14,12 @@ import java.util.*;
 public class Training {
     private Map<String, Integer> trainSpamFreq;
     private Map<String, Integer> trainHamFreq;
-    private Map<String, Integer> probSpam;
-    private Map<String, Integer> probHam;
+    private Map<String, Double> probOfWord;
 
     public Training() {
         trainSpamFreq = new TreeMap<>();
         trainHamFreq = new TreeMap<>();
-        probHam = new TreeMap<>();
-        probSpam = new TreeMap<>();
+        probOfWord = new TreeMap<>();
     }
 
     public void processFile(File file, Map<String, Integer> wordCounts) throws IOException {
@@ -60,40 +58,58 @@ public class Training {
         return false;
     }
 
-//    public void printWordCounts(int minCount, File outputFile) throws FileNotFoundException {
-//        System.out.println("Saving word counts to " + outputFile.getAbsolutePath());
-//        if (!outputFile.exists() || outputFile.canWrite()) {
-//            PrintWriter fout = new PrintWriter(outputFile);
-//
-//            Set<String> keys = wordCounts.keySet();
-//            Iterator<String> keyIterator = keys.iterator();
-//
-//            while(keyIterator.hasNext()) {
-//                String key = keyIterator.next();
-//                int count = wordCounts.get(key);
-//
-//                if (count >= minCount) {
-//                    fout.println(key + ": " + count);
-//                }
-//            }
-//            fout.close();
-//        } else {
-//            System.err.println("Cannot write to output file");
-//        }
-//    }
-    public void setProbSpam(int numSpam, Map<String, Integer> trainSpamFreq){
-        Set<String> key = trainSpamFreq.keySet();
-        Iterator<String> keyIterator = key.iterator();
+    public void printWordCounts(int minCount, File outputFile) throws FileNotFoundException {
+        System.out.println("Saving word counts to " + outputFile.getAbsolutePath());
+        if (!outputFile.exists() || outputFile.canWrite()) {
+            PrintWriter fout = new PrintWriter(outputFile);
 
-        while(keyIterator.hasNext()){
-            String num = keyIterator.next();
-            int count = trainSpamFreq.get(num);
+            Set<String> keys = probOfWord.keySet();
+            Iterator<String> keyIterator = keys.iterator();
 
-            
+            while(keyIterator.hasNext()) {
+                String key = keyIterator.next();
+                double count = probOfWord.get(key);
+
+                if (count >= minCount) {
+                    fout.println(key + ": " + count);
+                }
+            }
+            fout.close();
+        } else {
+            System.err.println("Cannot write to output file");
         }
     }
 
 
+    public void setProbOfWord(Map<String, Integer> spam, Map<String, Integer> ham, int numHam, int numSpam){
+        Iterator entries = spam.entrySet().iterator();
+
+        while(entries.hasNext()){
+            Map.Entry entry = (Map.Entry) entries.next();
+            String key = (String) entry.getKey();
+            Integer value = (Integer) entry.getValue();
+            if(ham.get(key) == null){
+                double probSpam = value/numSpam;
+                double prob = probSpam/(probSpam);
+                probOfWord.put(key, prob);
+                System.out.println(key + " " + prob);
+            }
+            else {
+                double probHam = ham.get(key) / numHam;
+                System.out.println(probHam);
+
+                double probSpam = value/numSpam;
+                double prob = probSpam/(probSpam + probHam);
+                probOfWord.put(key, prob);
+                System.out.println(key + " " + prob);
+
+            }
+
+
+
+
+        }
+    }
 
     public void train(File dir){
         File spam = new File(dir, "/spam");
@@ -101,6 +117,8 @@ public class Training {
         File ham2 = new File(dir, "/ham2");
         int numSpam = spam.listFiles().length;
         int numHam = ham.listFiles().length + ham2.listFiles().length;
+        System.out.println(numSpam + " " + numHam
+        );
         try {
             processFile(spam, trainSpamFreq);
             processFile(ham, trainHamFreq);
@@ -112,6 +130,16 @@ public class Training {
             e.printStackTrace();
         }
 
-
+        setProbOfWord(trainSpamFreq, trainHamFreq, numHam, numSpam);
+        File prob = new File("probability.txt");
+        try {
+            printWordCounts(2, prob);
+        }
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
