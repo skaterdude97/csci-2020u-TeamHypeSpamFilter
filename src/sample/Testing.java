d0e037ad7a3e11811f1a485959476499dc5fc75b
Andrei Stoica <andrei.stoica@uoit.net>
2016-03-10 19:10:11 -0500

    added Blacklisting code to Testing class

--- src/sample/Testing.java
+++ src/sample/Testing.java
@@ -2,6 +2,7 @@ package sample;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Map;
 import java.util.Scanner;
 import javafx.collections.*;
@@ -15,17 +16,22 @@ public class Testing {
     private File spam, ham;
     private int correctSpam=0, spamCount=0;
     private double accuracy, precision;
+    private BlackListEmail blackList;
 
     public Testing(File dir) {
         spam = new File(dir, "/spam");
         ham = new File(dir, "/ham");
+        blackList = new BlackListEmail();
     }
 
     public double getAccuracy () {return accuracy;}
 
     public double getPrecision () {return precision;}
 
-
+    public void addToBlacklist (String email) throws IOException {
+        if (!blackList.addEmail(ham, email))
+            blackList.addEmail(spam,email);
+    }
 
     private boolean isWord(String str){
         String pattern = "^[a-zA-Z]*$";
@@ -39,53 +45,71 @@ public class Testing {
         try {
 
             for (int i = 0; i<spam.listFiles().length; i++) {
-                Scanner testInput = new Scanner(spam.listFiles()[i]);
-                double sp;
-                double n = 0;
-                while (testInput.hasNext()) {
-                    String word = testInput.next();
-                    if (isWord(word)) {
-                        if (probOfWord.containsKey(word)) {
-                            if (probOfWord.get(word) == 1) {
-                                n += (Math.log(1 - 0.99999999999) - Math.log(0.9999999999));
-                            } else if (probOfWord.get(word)!= 0) {
-                                n += (Math.log(1 - probOfWord.get(word)) - Math.log(probOfWord.get(word)));
-                            } else {
-                                n+= (Math.log(1 -0.00000000001) - Math.log(0.00000000001));
+                if (!blackList.getBlacklist().contains(spam.listFiles()[i])) {
+                    Scanner testInput = new Scanner(spam.listFiles()[i]);
+                    double sp;
+                    double n = 0;
+                    while (testInput.hasNext()) {
+                        String word = testInput.next();
+                        if (isWord(word)) {
+                            if (probOfWord.containsKey(word)) {
+                                if (probOfWord.get(word) == 1) {
+                                    n += (Math.log(1 - 0.99999999999) - Math.log(0.9999999999));
+                                } else if (probOfWord.get(word) != 0) {
+                                    n += (Math.log(1 - probOfWord.get(word)) - Math.log(probOfWord.get(word)));
+                                } else {
+                                    n += (Math.log(1 - 0.00000000001) - Math.log(0.00000000001));
+                                }
                             }
                         }
                     }
+
+                    sp = 1 / (1 + Math.pow(Math.E, n));
+
+                    if (sp > 0.5) {
+                        correctSpam++;
+                        spamCount++;
+                    }
+
+                    files.add(new TestFile(spam.listFiles()[i].getName(), sp, "Spam"));
+                } else {
+                    correctSpam++;
+                    spamCount++;
+                    files.add(new TestFile(spam.listFiles()[i].getName(), 1.0, "Spam"));
                 }
-                sp = 1/(1+Math.pow(Math.E,n));
-
-                if (sp>0.5) {correctSpam++;spamCount++;}
-
-
-                files.add(new TestFile(spam.listFiles()[i].getName(),sp,"Spam"));
             }
             for (int i = 0; i<ham.listFiles().length; i++) {
-                Scanner testInput = new Scanner(ham.listFiles()[i]);
-                double sp;
-                double n = 0;
-                while (testInput.hasNext()) {
-                    String word = testInput.next();
-                    if (isWord(word)) {
-                        if (probOfWord.containsKey(word)) {
-                            if (probOfWord.get(word) == 1) {
-                                n += (Math.log(1 - 0.99999999999) - Math.log(0.9999999999));
-                            } else if (probOfWord.get(word)!= 0) {
-                                n += (Math.log(1 - probOfWord.get(word)) - Math.log(probOfWord.get(word)));
-                            } else {
-                                n += (Math.log(1 - 0.00000000001) - Math.log(0.00000000001));
+                if (!blackList.getBlacklist().contains(spam.listFiles()[i])) {
+                    Scanner testInput = new Scanner(ham.listFiles()[i]);
+                    double sp;
+                    double n = 0;
+                    while (testInput.hasNext()) {
+                        String word = testInput.next();
+                        if (isWord(word)) {
+                            if (probOfWord.containsKey(word)) {
+                                if (probOfWord.get(word) == 1) {
+                                    n += (Math.log(1 - 0.99999999999) - Math.log(0.9999999999));
+                                } else if (probOfWord.get(word) != 0) {
+                                    n += (Math.log(1 - probOfWord.get(word)) - Math.log(probOfWord.get(word)));
+                                } else {
+                                    n += (Math.log(1 - 0.00000000001) - Math.log(0.00000000001));
+                                }
                             }
                         }
                     }
+                    sp = 1 / (1 + Math.pow(Math.E, n));
+
+                    if (sp > 0.5) {
+                        spamCount++;
+                    }
+
+                    files.add(new TestFile(ham.listFiles()[i].getName(), sp, "Ham"));
+                } else {
+                    correctSpam++;
+                    spamCount++;
+                    files.add(new TestFile(spam.listFiles()[i].getName(), 1.0, "Ham"));
                 }
-                sp = 1/(1+Math.pow(Math.E,n));
 
-                if (sp>0.5) {spamCount++;}
-
-                files.add(new TestFile(ham.listFiles()[i].getName(), sp,"Ham"));
             }
 
             accuracy = 1-((spamCount-correctSpam)/(double)files.size());
