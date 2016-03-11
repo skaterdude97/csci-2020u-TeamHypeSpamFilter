a5fa8f0e82dab1f2a154f88932a569695b9cf664
Andrei Stoica <andrei.stoica@uoit.net>
2016-03-10 19:10:42 -0500

    finnished BlackListEmail class'

--- src/sample/BlackListEmail.java
+++ src/sample/BlackListEmail.java
@@ -2,45 +2,44 @@ package sample;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by brad on 10/03/16.
  */
 public class BlackListEmail {
-    private List<String> blacklist;
+    private List<File> blackList;
 
     public BlackListEmail() {
-        blacklist = new ArrayList<>();
+        blackList = new ArrayList<>();
 
     }
 
-    public void addEmail(String email){
-        blacklist.add(email);
+    public boolean addEmail(File dir, String email) throws IOException{
+            return processFile(dir,email);
     }
 
-    public List<String> getBlacklist () {
-        return blacklist;
+    public List<File> getBlacklist () {
+        return blackList;
     }
-//    public void processFile(File file, String[] blacklist) throws IOException {
-//        if (file.isDirectory()) {
-//            File[] filesInDir = file.listFiles();
-//            for (int i = 0; i < filesInDir.length; i++) {
-//                processFile(filesInDir[i], blacklist);
-//            }
-//        } else if (file.exists()) {
-//            Scanner scan = new Scanner(file);
-//            for (int i = 0; i < blacklist.length; i++) {
-//                while (scan.hasNextLine()) {
-//                    if (scan.nextLine().contains(blacklist[i])){
-//                        fileBlackList.add(file);
-//                    }
-//                }
-//            }
-//            }
-//        }
+
+   private boolean processFile(File file, String email) throws IOException {
+        boolean added = false;
+        if (file.isDirectory()) {
+            File[] filesInDir = file.listFiles();
+            for (int i = 0; i < filesInDir.length; i++) {
+                processFile(filesInDir[i], email);
+            }
+        } else if (file.exists()) {
+            Scanner scan = new Scanner(file);
+            while (scan.hasNextLine()) {
+                if (scan.nextLine().contains(email)){
+                    blackList.add(file);
+                    added = true;
+                }
+            }
+        }
+        return added;
+        }
 
 }
